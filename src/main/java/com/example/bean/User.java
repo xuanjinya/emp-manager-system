b7baf63f35e0b098e771fcca38c86f81491a8013
xuanjinya <1067998193@qq.com>
2021-09-09 13:27:15 +0800

    Initial commit

---
+++ src/main/java/com/example/bean/User.java
@@ -0,0 +1,14 @@
+package com.example.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
+
